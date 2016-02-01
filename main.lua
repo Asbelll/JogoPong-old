574e99164c523820c521ccb0684427a6da4d5318
Matheus Reis <matheus.reis@pedreira.org>
2016-02-01 14:34:01 -0200

    Cria arquivo de execução
    Foi adicionado arquivo inicial para o desenvolvimento do jogo.

---
+++ main.lua
@@ -0,0 +1,4 @@
+function love.draw()
+    love.graphics.print("Hello World", 400, 300)
+    love.graphics.print("Compre a DLC 'Partiu ser op' para adquirir pontos de exclamação em seu jogo", 200, 350)
+end
