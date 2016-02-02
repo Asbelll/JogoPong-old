ed4cd36aadfcac420e0ceffcf1eb9d2c402af7d1
Matheus Reis <matheus.reis@pedreira.org>
2016-02-02 14:50:35 -0200

    Adiciona um chamado à biblioteca cupid

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+require("lib/cupid");
+
 function love.draw()
     love.graphics.print("Hello World", 400, 300)
     love.graphics.print("Compre a DLC 'Partiu ser op' para adquirir pontos de exclamação em seu jogo", 200, 350)
