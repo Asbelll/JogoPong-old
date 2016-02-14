36484f4fcea1be7505aa67397677877ca781a850
Matheus Reis <goodnews.room@hotmail.com>
2016-02-13 23:28:27 -0200

    Adiciona um novo game state
    StartSequence contém todo o script a ser rodado assim que o jogo
    e será necessário apresentar algo ao jogador.

---
+++ states/StartSequence.lua
@@ -0,0 +1,47 @@
+-- <Descrição do state>
+
+-- Criação da tabela
+StartSequence = {}
+
+function StartSequence:new()
+   local gs = {}
+
+   gs = setmetatable(gs, self)
+   self.__index = self
+   _gs = gs
+
+   return gs
+end
+
+function StartSequence:load()
+end
+
+function StartSequence:close()
+end
+
+function StartSequence:enable()
+end
+
+function StartSequence:disable()
+end
+
+function StartSequence:update(dt)
+	xucrute = xucrute + dt
+end
+
+function StartSequence:draw()
+	love.graphics.print("Hello World", 400, 300)
+	love.graphics.print("Compre a DLC 'Partiu ser op' para adquirir pontos de exclamação em seu jogo", 200, 350)
+end
+
+function StartSequence:keypressed(key, unicode)
+end
+
+function StartSequence:keyreleased(key, unicode)
+end
+
+function StartSequence:mousepressed(x, y, button)
+end
+
+function StartSequence:mousereleased(x, y, button)
+end
