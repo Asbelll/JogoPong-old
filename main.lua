854fae392b7b1ed3f30694a3565677218ad1a89b
Matheus Reis <goodnews.room@hotmail.com>
2016-02-13 23:20:39 -0200

    Adiciona as instruções iniciais
    Adiciona instruções para iniciar no modo debug e para chamar funções
    dos states de forma dinâmica.

--- main.lua
+++ main.lua
@@ -1,6 +1,47 @@
-require("lib/cupid");
+-- Importa todos os game states
+require("states/StartSequence")
+
+function love.load()
+	-- Verifica os argumentos de inicialização escolher o modo de execução
+	debugMode = false
+
+	for l = 1, #arg do
+		if (arg[l] == "-KelverMode") then
+			-- Inicia no modo debug
+			require("lib/cupid")
+			lovebird = require("lib/lovebird")
+
+			debugMode = true
+		end
+	end
+	-- Adiciona os game states para uso futuro.
+	addState(StartSequence, "StartSequence")
+end
+
+function love.update(dt)
+	if (debugMode) then
+		lovebird.update(dt)
+	end
+
+	lovelyMoon.update(dt)
+end
 
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
-    love.graphics.print("Compre a DLC 'Partiu ser op' para adquirir pontos de exclamação em seu jogo", 200, 350)
+	lovelyMoon.draw()
+end
+
+function love.keypressed(key, unicode)
+	lovelyMoon.keypressed(key, unicode)
+end
+
+function love.keyreleased(key, unicode)
+	lovelyMoon.keyreleased(key, unicode)
+end
+
+function love.mousepressed(x, y, button)
+	lovelyMoon.mousepressed(x, y, button)
+end
+
+function love.mousereleased(x, y, button)
+	lovelyMoon.mousereleased(x, y, button)
 end
