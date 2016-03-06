f967c26aa8b51d1dda33ad701d50197647d7d1ec
Matheus Reis <goodnews.room@hotmail.com>
2016-03-05 23:04:03 -0300

    Remove variável
    Remove a variável debugMode que deixou de ser utilizada no projeto.

--- conf.lua
+++ conf.lua
@@ -4,9 +4,6 @@ require("lib/stateManager")
 require("lib/lovelyMoon")
 require("lib/cupid")
 
--- Definições padrões de inicialização
-debugMode = false
-
 function love.conf(t)
 	t.version = "0.9.2"
 	t.window.title = "JogoNave (Yup, melhor nome ever)"
