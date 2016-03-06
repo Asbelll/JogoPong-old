9719f210af51bfdf15deb6fd982c1ff27ed37db9
Matheus Reis <goodnews.room@hotmail.com>
2016-03-06 16:51:40 -0300

    Atualiza nome de métodos

--- states/StartSequence.lua
+++ states/StartSequence.lua
@@ -1,5 +1,3 @@
--- <Descrição do state>
-
 -- Criação da tabela
 class.StartSequence()
 
@@ -10,8 +8,6 @@ function StartSequence:close()
 end
 
 function StartSequence:enable()
-	button = loveframes.Create("button")
-	button:SetSize(100, 100):SetText("Asbels2Razz"):SetPos(330, 35)
 end
 
 function StartSequence:disable()
@@ -21,12 +17,10 @@ function StartSequence:update(dt)
 end
 
 function StartSequence:draw()
-	love.graphics.print("Hello World", 400, 300)
-	love.graphics.print("Compre a DLC 'Partiu ser op' para adquirir pontos de exclamação em seu jogo", 200, 350)
 end
 
-function StartSequence:buttonpressed(key, isrepeat)
+function StartSequence:keypressed(key, isrepeat)
 end
 
-function StartSequence:buttonreleased(key)
+function StartSequence:keyreleased(key)
 end
