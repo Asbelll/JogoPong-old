4b92fb898025b0355384657a4028f4bae23d3609
Matheus Reis <goodnews.room@hotmail.com>
2016-03-06 16:48:48 -0300

    Importa binser ao projeto

--- conf.lua
+++ conf.lua
@@ -3,8 +3,9 @@ require 'lib/pl'
 require("lib/stateManager")
 require("lib/lovelyMoon")
 require("lib/cupid")
+binser = require "lib/binser"
 
 function love.conf(t)
 	t.version = "0.9.2"
-	t.window.title = "JogoNave (Yup, melhor nome ever)"
+	t.window.title = "JogoNave"
 end
