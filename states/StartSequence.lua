eda0302c6d1a1ca7817bffb669a02287229ba0df
Asbelll <chrystian_alex@hotmail.com>
2016-03-19 11:51:58 -0300

    Cria Left Paddle e movimento
    Criado Left Paddle e movimento vertical do mesmo

--- states/StartSequence.lua
+++ states/StartSequence.lua
@@ -1,6 +1,8 @@
 -- Criação da tabela
 class.StartSequence()
 
+LPaddle = {x = 50, y = love.graphics.getHeight()/2 - 45, speed = 0, speedMax = 150}
+
 function StartSequence:load()
 end
 
@@ -14,12 +16,39 @@ function StartSequence:disable()
 end
 
 function StartSequence:update(dt)
+	if LPaddle.y >= 0 and LPaddle.y <= (love.graphics.getHeight() - 90) then
+		LPaddle.y = LPaddle.y + (LPaddle.speed*dt)
+	end
+
+	if LPaddle.y < 0 then
+		LPaddle.y = 0
+	elseif LPaddle.y > (love.graphics.getHeight() - 90) then
+		LPaddle.y = love.graphics.getHeight() - 90
+	end
+
+	if LPaddle.speed > 0 then
+		LPaddle.speed = LPaddle.speed - 3,125
+	elseif LPaddle.speed < 0 then
+		LPaddle.speed = LPaddle.speed + 3,125
+	end
 end
 
 function StartSequence:draw()
+	love.graphics.rectangle("fill", LPaddle.x, LPaddle.y, 10, 90, 0, 0, 0 )
 end
 
 function StartSequence:keypressed(key, isrepeat)
+	if key == "LPaddleUp" then
+		if LPaddle.speed > (LPaddle.speedMax*-1) then
+			LPaddle.speed = LPaddle.speed + -50
+		end
+	end
+
+	if key == "LPaddleDown" then
+		if LPaddle.speed < LPaddle.speedMax then
+			LPaddle.speed = LPaddle.speed + 50
+		end
+	end
 end
 
 function StartSequence:keyreleased(key)
