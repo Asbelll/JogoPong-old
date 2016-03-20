2738903034e1035cb6d2968a7a39b3e67abd0509
Matheus Reis <goodnews.room@hotmail.com>
2016-03-20 20:04:42 -0300

    Modifica valores de inicialização

--- classes/Ball.lua
+++ classes/Ball.lua
@@ -1,18 +1,10 @@
 class.Ball()
 
---[[
-[22:53:22] Matheus: Yf = (velocidadeDoPaddle + [PP] + Y)
-[22:53:43 | Edited 23:00:08] Matheus: Xf = X - (|Yf| - |Y|)
-[22:54:34] Matheus: Limitar velocidade mínima de X
-]]--
-
-math.randomseed(os.time())
-
-function Ball:_init(y)
+function Ball:_init(speedY)
 	self.x = love.graphics.getWidth()/2
-	self.y = y
+	self.y = love.graphics.getWidth()/2
 	self.speedX = 250
-	self.speedY = 0
+	self.speedY = speedY
 	self.radius = 10
 	self.direct = math.random(2)
 end
