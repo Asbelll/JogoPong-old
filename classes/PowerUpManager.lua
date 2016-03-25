b50d4ad612048df98c106abf0eacc7643573151e
Matheus Reis <goodnews.room@hotmail.com>
2016-03-24 21:44:55 -0300

    Correção de bugs

--- classes/PowerUpManager.lua
+++ classes/PowerUpManager.lua
@@ -1,26 +1,31 @@
 class.PowerUpManager()
 
 function PowerUpManager:_init()
-	self.powerUpList = {puEnlarge(), PowerUp()}
-	self.active = 1
+	self.powerUpList = {puEnlarge()}
+	self.active = 0
 	self.enabled = {}
 end
 
 function PowerUpManager:draw()
-	-- Desenha os power ups ativos
-	self.powerUpList[self.active]:draw()
+	if (self.active ~= 0) then
+		-- Desenha os power ups ativos
+		self.powerUpList[self.active]:draw()
+	end
 end
 
 function PowerUpManager:update(dt)
 	-- Realiza operações no power up ativo.
-	if (not self.powerUpList[self.active].enabled) then
-		-- Move os power ups ativos na tela.
-		self.powerUpList[self.active]:move(dt)
+	if (self.active ~= 0) then
+		if (not self.powerUpList[self.active].enabled) then
+			-- Move os power ups ativos na tela.
+			self.powerUpList[self.active]:move(dt)
 
-		-- Verifica se houve colisão entre a bolinha e o power up.
-		if (Hit:checkCollision(ball.hitbox, self.powerUpList[self.active].hitbox)) then
-			self.powerUpList[self.active]:enable()
-			self.enabled[#self.enabled] = self.active
+			-- Verifica se houve colisão entre a bolinha e o power up.
+			if (Hit:checkCollision(ball.hitbox, self.powerUpList[self.active].hitbox)) then
+				self.powerUpList[self.active]:enable()
+				self.enabled[#self.enabled] = self.active
+				self.active = 0
+			end
 		end
 	end
 
@@ -36,13 +41,11 @@ function PowerUpManager:update(dt)
 		end
 
 		-- Atualiza power ups ainda ativados.
-		self.powerUpList[indicePowerUp]:update()
-
-		pretty.dump(self.enabled)
+		self.powerUpList[indicePowerUp]:update(dt)
 	end
 end
 
 function PowerUpManager:newPowerUp()
-	self.active = math.random(1, table.getn(self.powerUpList))
-	self.powerUpList[self.active]:activate()
+		self.active = math.random(1, #self.powerUpList)
+		self.powerUpList[self.active]:activate()
 end
