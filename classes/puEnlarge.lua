10ca5dbe3c9bd699cbdfa8ad21183cad8afbcc87
Matheus Reis <goodnews.room@hotmail.com>
2016-03-24 21:45:19 -0300

    Adição de eventos

--- classes/puEnlarge.lua
+++ classes/puEnlarge.lua
@@ -2,3 +2,10 @@ class.puEnlarge(PowerUp)
 
 puEnlarge.imageDir = "images/puEnlarge.png"
 
+function puEnlarge:onEnable()
+	self.target.height = 150
+end
+
+function puEnlarge:onDisable()
+	self.target.height = 90
+end
