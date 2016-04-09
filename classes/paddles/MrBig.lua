8dad7579710b0fcca94a583e2d0837adba2501fe
Matheus Reis <goodnews.room@hotmail.com>
2016-04-09 19:48:15 -0300

    Cria estrutura base da classe

---
+++ classes/paddles/MrBig.lua
@@ -0,0 +1,11 @@
+class.MrBig(Paddle)
+
+-- Propriedades padrões
+MrBig.id = "MrBig" -- Valor identificador do paddle.
+MrBig.name = "Mr. Big"
+MrBig.height = 150
+MrBig.accel = 5.4
+MrBig.speedMax = 855
+MrBig.strength = 50
+MrBig.friction = 4.5
+MrBig.color = {r = 200, g = 10, b = 255, a = 255}
