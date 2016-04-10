a438fd79f6f6acc5f54f0e720747efc2953ea224
Matheus Reis <goodnews.room@hotmail.com>
2016-04-10 11:27:21 -0300

    Faz o gfx pertencer aos objetos

--- classes/skills/Meter.lua
+++ classes/skills/Meter.lua
@@ -1,38 +1,42 @@
 class.Meter()
 
 Meter.activationMode = "keyPress"
-Meter.energyMax = 1000
+Meter.energyMax = 240
 Meter.stocks = 1
-local gfx  = {}
 
 function Meter:_init(id)
 	self.energy = 0
 	self.id = id
-	gfx.bg = love.graphics.newImage("images/meters/" .. self.id .. "/bg.png")
-	gfx.energy = love.graphics.newImage("images/meters/" .. self.id .. "/energy.png")
-	gfx.division = love.graphics.newImage("images/meters/" .. self.id .. "/division.png")
-	gfx.border = love.graphics.newImage("images/meters/" .. self.id .. "/border.png")
+	self.gfx = {}
+	self.gfx.bg = love.graphics.newImage("images/meters/" .. id .. "/bg.png")
+	self.gfx.energy = love.graphics.newImage("images/meters/" .. id .. "/energy.png")
+	self.gfx.division = love.graphics.newImage("images/meters/" .. id .. "/division.png")
+	self.gfx.border = love.graphics.newImage("images/meters/" .. id .. "/border.png")
+	self.gfx.aff = "images/meters/" .. id .. "/border.png"
 end
 
 function Meter:update(dt)
+	if (self.energy < 250) then
+		self.energy = self.energy + dt
+	end
 end
 
 function Meter:draw()
 	local imageX
-	local imageY = love.graphics.getHeight()/2 - gfx.bg:getHeight()/2
+	local imageY = love.graphics.getHeight()/2 - self.gfx.bg:getHeight()/2
 	local parentPaddle = Paddle.getPaddleBySide(self.side)
-	local energyQuad = love.graphics.newQuad(0, 0, gfx.energy:getWidth(), self.energy, gfx.energy:getDimensions())
+	local energyQuad = love.graphics.newQuad(0, 0, self.gfx.energy:getWidth(), self.energy, self.gfx.energy:getDimensions())
 
 	if (self.side == "L") then
-		imageX = (50 - gfx.bg:getWidth())/2 -- Posição central entre o paddle e a extremidade da janela
+		imageX = (50 - self.gfx.bg:getWidth())/2 -- Posição central entre o paddle e a extremidade da janela
 	else
-		imageX = love.graphics.getWidth() - ((50 - gfx.bg:getWidth())/2 + parentPaddle.width + parentPaddle.width) -- Posição central entre o paddle e a extremidade da janela
+		imageX = love.graphics.getWidth() - ((50 - self.gfx.bg:getWidth())/2 + parentPaddle.width + parentPaddle.width) -- Posição central entre o paddle e a extremidade da janela
 	end
 
-	love.graphics.draw(gfx.bg, imageX, imageY)
-	love.graphics.draw(gfx.energy, energyQuad, imageX, imageY)
-	love.graphics.draw(gfx.division, imageX, imageY)
-	love.graphics.draw(gfx.border, imageX, imageY)
+	love.graphics.draw(self.gfx.bg, imageX, imageY)
+	love.graphics.draw(self.gfx.energy, energyQuad, imageX, imageY)
+	love.graphics.draw(self.gfx.division, imageX, imageY)
+	love.graphics.draw(self.gfx.border, imageX, imageY)
 end
 
 -- Eventos --
