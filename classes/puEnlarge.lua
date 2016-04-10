9f3c18e8cb923d9ceb5ac129f9b1cb6f0eab9fd4
Matheus Reis <goodnews.room@hotmail.com>
2016-04-09 22:01:58 -0300

    Atualiza cálculo de diminuição/aumento da altura

--- classes/puEnlarge.lua
+++ classes/puEnlarge.lua
@@ -5,9 +5,9 @@ puEnlarge.duration = 20
 puEnlarge.enableSounds = {"sounds/enlarge.ogg"}
 
 function puEnlarge:onEnable()
-	self.target.height = self.target.height + 60
+	self.target.height = self.target.height + self.target.pHeight/3 * 2 -- Paddle ganha 2/3 de sua altura base.
 end
 
 function puEnlarge:onDisable()
-	self.target.height = self.target.height - 60
+	self.target.height = self.target.height - self.target.pHeight/3 * 2 -- Paddle perde 2/3 de sua altura base.
 end
--- classes/puShorten.lua
+++ classes/puShorten.lua
@@ -6,9 +6,9 @@ puShorten.targetType = "reverse"
 puShorten.enableSounds = {"sounds/shorten.ogg"}
 
 function puShorten:onEnable()
-	self.target.height = self.target.height - 60
+	self.target.height = self.target.height - self.target.pHeight/3 * 2 -- Paddle perde 2/3 de sua altura base.
 end
 
 function puShorten:onDisable()
-	self.target.height = self.target.height + 60
+	self.target.height = self.target.height + self.target.pHeight/3 * 2 -- Paddle ganha 2/3 de sua altura base.
 end
