c4f5597a89c61e27d048f75ee12423a24f79dc19
Matheus Reis <goodnews.room@hotmail.com>
2016-04-17 02:58:16 -0300

    Cria classe de base para o efeito fireball

---
+++ particleEfx/pFireball.lua
@@ -0,0 +1,17 @@
+class.pFireball()
+
+function pFireball:_init()
+	self.object = {}
+end
+
+function pFireball:onEnable()
+end
+
+function pFireball:onDisable()
+end
+
+function pFireball:update(dt)
+end
+
+function pFireball:draw()
+end
